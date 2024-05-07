0fe76d5b1c82da685e46be358303cc55b1e68f7b
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-07 10:00:16 -0400

    Added functions

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callbackFunction){
+    return callbackFunction();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+    }
+}
